9807fe900c0a9ead76ca1ca4fd416e88a1606818
LuizHNR <luizhneri12@gmail.com>
2024-08-20 15:34:01 -0300

    Variaveis JS

--- Front/estudos-HTML-CSS-JS/js/script.js
+++ Front/estudos-HTML-CSS-JS/js/script.js
@@ -1,4 +1,4 @@
-//------------------------------------------Horas em js-----------------------------------------------------------------
+//-------------------------------------------------Horas em js------------------------------------------------------------
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
@@ -17,3 +17,50 @@ var el = document.getElementById("hora");
 el.textContent = greeting;
 
 //-----------------------------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------Variaveis e tipos-------------------------------------------------------
+let nome; //Cria uma variavel com valor indefinido
+let disciplina = null; // Cria uma variável vazia
+let texto = "Este é um exemplo"; // Cria uma variável com um valor
+
+let meuNome = "luiz Henrique"; // {string} = Variaveis de texto. elas devem ser representadas entre aspas
+let minhaIdade = 18; // {int} = números inteiros
+let valorProdutos = 5.35; // {float} = Numeros flutuantes com casas decimais
+let namorando = true; // {booleanos} = Que recebe os valores falso ou verdadeiro
+
+let linguagens = ["HTML", "CSS", "JAVA SCRIPT"] // {Arrays} = Conjunto de elementos
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------Objetos-------------------------------------------------------------
+
+let aluno = {
+    nome: "Luiz",
+    idade: 18,
+    estadoCivil: "solteiro",
+    estado: "Estudando",
+    aviso: function(){
+        alert("!!!Não usar alert por ser utrapassado!!!")
+    }
+}
+
+console.log(aluno.nome);
+aluno.aviso()
+
+//------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------Manipulando Textos e Números------------------------------------------------
+
+//---Para mudar o tipo da variavel para int ou float utiliza {parse-int ou float-(nome da variavel)}---
+let numFloat = 123.456
+console.log(parseInt(numFloat))
+
+let numString = "123.456"
+console.log(parseFloat(numString))
+
+//---Para mudar o tipo da variavel para string utiliza {toString(nome da variavel)}---
+
+let nmFloat = 123.456
+console.log(toString(nmFloat))
+
+//------------------------------------------------------------------------------------------------------------------------
