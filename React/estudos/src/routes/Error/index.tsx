f3ec7290ee7d4ff57aa71d6263bb70170ed45f47
LuizHNR <luizhneri12@gmail.com>
2024-09-11 21:20:24 -0300

    routes

---
+++ React/estudos/src/routes/Error/index.tsx
@@ -0,0 +1,8 @@
+export default function Error(){
+
+    return(
+        <div>
+            <h1>Error 404 - Página não encontrada</h1>
+        </div>
+    )
+}
---
+++ React/estudos/src/routes/Usuario/index.tsx
@@ -0,0 +1,7 @@
+export default function Usuario() {
+    return (
+        <div>
+            <h1>Usuário</h1>
+        </div>
+    )
+}
