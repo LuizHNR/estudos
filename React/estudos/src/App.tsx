020bbd4c309fe45d6e5c145285d6ba09e65a87a8
LuizHNR <luizhneri12@gmail.com>
2024-09-11 21:33:31 -0300

    components e chamada

--- React/estudos/src/App.tsx
+++ React/estudos/src/App.tsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Cabecalho from "./routes/components/Cabecalho/Cabecalho";
+import Rodape from "./routes/components/Rodape/Rodape";
+
 export default function App() {
   return (
 
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Cabecalho/>
+      <Outlet/>
+      <Rodape/>
+    </>
 
   );
 }
---
+++ React/estudos/src/routes/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+export default function Rodape(){
+
+    return(
+        <div>
+            <h1>Rodape</h1>
+        </div>
+    )
+}
